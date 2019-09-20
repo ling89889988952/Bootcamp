8637c53bc4c913f16a26bbee364f7105e001abdc
夏鸣谦 <xiamingqian@xiamingqiandeMacBook-Pro.local>
2019-09-20 15:55:12 -0400

    change database

--- js/module/dataObject.js
+++ js/module/dataObject.js
@@ -1,22 +1,23 @@
 const Members = {
-    LingZou:{
-        LastName:"Zou",
-        FirstName:"Ling",
-        Role:"Web Developer",
-        Experience:"Receive professional training and rich experience in web development",
-        Bio:"Through a professional website building system to Integrate the company's cultural and products to build the online platform. And through data collection and analysis to help companies get more target customers.",
-        Avator:""
+    LingZou: {
+        LastName: "Zou",
+        FirstName: "Ling",
+        Role: "Web Developer",
+        Experience: "Receive professional training and rich experience in web development",
+        Bio: "Through a professional website building system to Integrate the company's cultural and products to build the online platform. And through data collection and analysis to help companies get more target customers.",
+        Avator: ""
     },
-    MingqianXia:{
-        LastName:"",
-        FirstName:"",
-        Role:"",
-        Experience:"",
-        Avator:""
+    MingqianXia: {
+        LastName: "Xia",
+        FirstName: "Mingqian",
+        Role: "Creative Director",
+        Experience: "Engaged in the design industry for 5 years",
+        Motto: "Bright design is not necessarily a good design, the best design is those that are suitable for the company and the product.",
+        Avator: ""
     }
-    
-    
-    
+
+
+
 
 }
 
