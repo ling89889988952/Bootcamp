317600b83d7582802bb09bc290c03271d399d280
ling89889988952 <zouling707@gmail.com>
2019-09-20 20:41:04 -0400

    update scss and js

--- js/module/dataObject.js
+++ js/module/dataObject.js
@@ -1,5 +1,32 @@
-const Members = {
-    LingZou: {
+const Team = {
+
+    Wang : {
+        LastName: "Wang",
+        FirstName:"Jianghaonan",
+        Role: "",
+        Experience: "",
+        Bio: "",
+        Avator: ""
+    },
+
+    Xia :{
+        LastName: "Xia",
+        FirstName: "Mingqian",
+        Role: "Creative Director",
+        Experience: "Engaged in the design industry for 5 years",
+        Motto: "Bright design is not necessarily a good design, the best design is those that are suitable for the company and the product.",
+        Avator: ""
+    },
+
+    Chen : {
+        LastName: "Boling Chen",
+        FirstName: "",
+        Role: "",
+        Experience: "",
+        Avator: ""
+    },
+
+    Zou : {
         LastName: "Zou",
         FirstName: "Ling",
         Role: "Web Developer",
@@ -7,18 +34,11 @@ const Members = {
         Bio: "Through a professional website building system to Integrate the company's cultural and products to build the online platform. And through data collection and analysis to help companies get more target customers.",
         Avator: ""
     },
-    MingqianXia: {
-        LastName: "Xia",
-        FirstName: "Mingqian",
-        Role: "Creative Director",
-        Experience: "Engaged in the design industry for 5 years",
-        Motto: "Bright design is not necessarily a good design, the best design is those that are suitable for the company and the product.",
-        Avator: ""
-    }
+  
 
 
 
 
 }
 
-export default Members;
+export default Team;
